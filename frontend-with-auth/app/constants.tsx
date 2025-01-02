2cc2ef6699d197dcbfca8f9ce4f46e8fe0c39df4
Ashutosh Shrivastava <ashutoshmishra422@gmail.com>
2025-01-02 11:50:02 +0000

    minor fixes

--- frontend-with-auth/app/constants.tsx
+++ frontend-with-auth/app/constants.tsx
@@ -1,79 +1,3 @@
-export const courseData = {
-  data: [
-    {
-      course_code: "COMP41730",
-      course_name: "Distributed Systems",
-      course_description: "Understanding the concepts of distributed systems.",
-    },
-    {
-      course_code: "COMP12345",
-      course_name: "Algorithms",
-      course_description:
-        "In-depth study of advanced algorithms and data structures.",
-    },
-    {
-      course_code: "COMP67890",
-      course_name: "Machine Learning",
-      course_description:
-        "A comprehensive introduction to machine learning concepts.",
-    },
-    {
-      course_code: "COMP41761",
-      course_name: "Computer Networks",
-      course_description: "Understanding the concepts of distributed systems.",
-    },
-    {
-      course_code: "COMP24563",
-      course_name: "Data Structures",
-      course_description:
-        "In-depth study of advanced algorithms and data structures.",
-    },
-  ],
-};
-
-export const courseDetails = {
-  course_code: "COMP41730",
-  course_name: "Distributed Systems",
-  course_description: "Understanding the concepts of distributed systems.",
-  topics: [
-    {
-      id: "1",
-      title: "Introduction to Distributed Systems",
-      description:
-        "Overview of distributed systems, their characteristics, and common use cases.",
-      pdfData: "intro-to-distributed-systems.pdf",
-    },
-    {
-      id: "2",
-      title: "Communication in Distributed Systems",
-      description:
-        "Exploring communication protocols, message passing, and challenges in distributed systems.",
-      pdfData: "communication-protocols.pdf",
-    },
-    {
-      id: "3",
-      title: "Consensus and Fault Tolerance",
-      description:
-        "Understanding consensus algorithms like Paxos and Raft, and techniques for achieving fault tolerance.",
-      pdfData: "consensus-fault-tolerance.pdf",
-    },
-    {
-      id: "4",
-      title: "Distributed Data Storage",
-      description:
-        "Introduction to distributed databases, consistency models, and CAP theorem.",
-      pdfData: "distributed-data-storage.pdf",
-    },
-    {
-      id: "5",
-      title: "Distributed System Design Patterns",
-      description:
-        "Common design patterns used in building distributed systems, such as microservices and event-driven architecture.",
-      pdfData: "design-patterns.pdf",
-    },
-  ],
-};
-
 export const notifications = [
   {
     id: 1,
@@ -114,8 +38,8 @@ export const DUMMY_QUIZZES = {
     {
       course_code: "COMP40073",
       quizzes: [
-        { quiz_name: "Midterm", status: "expired" },
-        { quiz_name: "Final", status: "pending" },
+        { quiz_name: "Quiz 1", status: "expired" },
+        { quiz_name: "Quiz 2", status: "pending" },
       ],
     },
     {
